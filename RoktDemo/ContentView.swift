cb4527a37def6e65cb6c73bd41930826f8c222f6
Danial Motahari <danial.motahari@rokt.com>
2021-03-25 14:01:31 +1100

    Initial Commit

---
+++ RoktDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RoktDemo
+//
+//  Created by Danial Motahari on 25/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
