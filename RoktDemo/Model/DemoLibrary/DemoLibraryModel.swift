fb1163cf1d1c3c7142f5cdec221a57e90a62ca0a
Danial Motahari <49017604+danial-rokt@users.noreply.github.com>
2021-11-25 15:23:15 +1100

    PreDefined page 2 created (#45)

--- RoktDemo/Model/DemoLibrary/DemoLibraryModel.swift
+++ RoktDemo/Model/DemoLibrary/DemoLibraryModel.swift
@@ -18,7 +18,7 @@ struct DemoLibraryModel: Codable {
     let demoDescription: String
     let defaultPlacementsExamples: DefaultPlacementExamplesModel
     let customConfigurationPage: CustomConfigurationPageModel
-    let preDefinedScreen1: PreDefinedScreenModel
-    let preDefinedScreen2: PreDefinedScreenModel
-    let preDefinedScreen3: PreDefinedScreenModel
+    let preDefinedScreen1: PreDefinedScreen1Model
+    let preDefinedScreen2: PreDefinedScreen2Model
+    let preDefinedScreen3: PreDefinedScreen3Model
 }
