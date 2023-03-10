355b7694e14862c8328805befe778d4ec0346862
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-03-10 15:03:13 +1100

    Script

---
+++ .buildkite/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+brew uninstall openssl --force
+brew install gnupg
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+source /Users/ec2-user/.rvm/scripts/rvm
+rvm use ruby-2.7.6@ios-demo --create
+curl -Lks 'https://git.io/rg-ssl' | ruby
