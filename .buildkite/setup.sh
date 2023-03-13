e7dc273bd820f4e69be38363ff44b4c3d3e3999d
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-03-14 10:35:11 +1100

    update cert

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-# source /Users/ec2-user/.rvm/scripts/rvm
-# rvm fix-permissions
-# ls -la /Users/ec2-user/.rvm
-# yes yes | rvm implode
-# brew uninstall openssl --force
-# gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-# curl -sSL https://get.rvm.io | bash -s stable
-# export LANG=en_US.UTF-8
-# export LANGUAGE=en_US.UTF-8
-# export LC_ALL=en_US.UTF-8
-# source /Users/ec2-user/.rvm/scripts/rvm
-# rvm install ruby-2.7.6
-# rvm use ruby-2.7.6@ios-demo --create
-# curl -Lks 'https://git.io/rg-ssl' | ruby
-which openssl
+!/bin/bash
+source /Users/ec2-user/.rvm/scripts/rvm
+rvm fix-permissions
+ls -la /Users/ec2-user/.rvm
+yes yes | rvm implode
+brew uninstall openssl --force
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+curl -sSL https://get.rvm.io | bash -s stable
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+source /Users/ec2-user/.rvm/scripts/rvm
+rvm osx-ssl-certs update all
+rvm install ruby-2.7.6
+rvm use ruby-2.7.6@ios-demo --create
+curl -Lks 'https://git.io/rg-ssl' | ruby
