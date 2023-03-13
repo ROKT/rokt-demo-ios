b8fb64750bd2e6bdeda6eb9c5d150097c5bbbc48
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-03-14 10:10:29 +1100

    implode

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /Users/ec2-user/.rvm/scripts/rvm
 rvm implode
 brew uninstall openssl --force
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
@@ -7,6 +8,6 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 source /Users/ec2-user/.rvm/scripts/rvm
-rvm install ruby-2.7.6 --disable-binary
+rvm install ruby-2.7.6
 rvm use ruby-2.7.6@ios-demo --create
 curl -Lks 'https://git.io/rg-ssl' | ruby
