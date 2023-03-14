8ae2243bc9e10a8724d9b57b7e6beb686de43961
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-03-14 13:54:53 +1100

    Passing ENV

--- .buildkite/publish.sh
+++ .buildkite/publish.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-source /Users/ec2-user/.rvm/scripts/rvm
-rvm osx-ssl-certs update all
-rvm use ruby-2.7.6@ios-demo --create
-bundle install
-xcrun simctl boot "iPhone 14"
-pod install --verbose
-bundle exec fastlane beta refresh_certificates:false use_temporary_keychain:true
-xcrun simctl shutdown "iPhone 14"
+echo $BUILDKITE_BUILD_NUMBER
