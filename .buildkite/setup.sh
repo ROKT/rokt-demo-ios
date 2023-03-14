8405be0527b296c4a10bcac58d0b0590e8a88d4c
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-03-14 12:11:10 +1100

    Test

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -3,4 +3,7 @@ source /Users/ec2-user/.rvm/scripts/rvm
 rvm osx-ssl-certs update all
 rvm use ruby-2.7.6@ios-demo --create
 bundle install
+xcrun simctl boot "iPhone 14"
 pod install --verbose
+bundle exec fastlane test
+xcrun simctl shutdown "iPhone 14"
