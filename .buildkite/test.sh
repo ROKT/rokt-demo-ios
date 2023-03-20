0e169339afeb0c286a05fad0326dd7ea1ed29ee6
activehigh <mahmudul.islam@rokt.com>
2023-03-20 21:54:19 +1100

    Mon Mar 20 21:54:19 AEDT 2023

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -2,8 +2,18 @@
 . ~/.zshrc
 rbenv local 2.7.6
 gem install bundler
+
+echo "Running bundle install..."
 bundle install
+
+echo "Starting iphone simulator..."
 xcrun simctl boot "iPhone 14"
+
+echo "Running cocoapod install..."
 pod install --verbose
+
+echo "Running bundle exec..."
 bundle exec fastlane test
+
+echo "Shutting down iphone 14"
 xcrun simctl shutdown "iPhone 14" || true
