528188b98bc4a05eeae9be3ea1c8abd95deca107
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-09-27 16:02:42 +1000

    Update versions

---
+++ .buildkite/bin/create_tag..sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+# $1 version
+
+git config user.email "buildkite@rokt.com"
+git config user.name "Buildkite"
+
+git add Podfile
+git add Podfile.lock
+git add RoktDemo.xcodeproj/project.pbxproj
+git commit -m "v$1"
+git tag -a "v$1" -m "Automated release v$1"
+git push origin "v$1"
