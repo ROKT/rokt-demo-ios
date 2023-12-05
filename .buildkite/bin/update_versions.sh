df29619a35069d148f5231b750e3d31c88d13002
Lewis Krishnamurti <lewis.raj.krishnamurti@rokt.com>
2023-12-05 17:05:01 +1100

    Remove optimistic operator from regex

--- .buildkite/bin/update_versions.sh
+++ .buildkite/bin/update_versions.sh
@@ -8,7 +8,7 @@ set -eu
 # $4 Demo app build number
 # $5 Project file location
 
-perl -pi -e "s/(?<='Rokt-Widget', '~> )(.*)(?=')/$1/g" $2
+perl -pi -e "s/(?<='Rokt-Widget', ')(.*)(?=')/$1/g" $2
 perl -pi -e "s/(?<=MARKETING_VERSION = )(.*)(?=;)/$3/g" $5
 perl -pi -e "s/(?<=CURRENT_PROJECT_VERSION = )(.*)(?=;)/$4/g" $5
 pod install --repo-update
