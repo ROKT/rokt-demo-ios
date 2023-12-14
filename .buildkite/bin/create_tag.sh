8a999b5a44d666ea03e5bc44ecb99a23c51abc0c
Lewis Krishnamurti <122851605+lewis-rokt@users.noreply.github.com>
2023-12-14 15:54:48 +1100

    [NO-JIRA] Add version code to tag (#91)

--- .buildkite/bin/create_tag.sh
+++ .buildkite/bin/create_tag.sh
@@ -3,14 +3,15 @@
 set -eu
 
 # $1 version
+# $2 version code
 
 git config user.email "buildkite@rokt.com"
 git config user.name "Buildkite"
 
-git tag -d "v$1" || true
+git tag -d "v$1-$2" || true
 git add Podfile
 git add Podfile.lock
 git add RoktDemo.xcodeproj/project.pbxproj
 git commit -m "v$1"
-git tag -a "v$1" -m "Automated release v$1"
-git push origin "v$1"
+git tag -a "v$1-$2" -m "Automated release v$1-$2"
+git push origin "v$1-$2"
